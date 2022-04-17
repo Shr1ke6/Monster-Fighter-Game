0939137e2942e96c7fba05d8e11e7a3eb4b4b275
qzh78 <qzh78@uclive.ac.nz>
2022-04-17 20:57:25 +1200

    added item

---
+++ src/Item.java
@@ -0,0 +1,14 @@
+
+public class Item {
+	boolean consumable;
+
+	public Item() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
