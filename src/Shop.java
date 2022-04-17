d0e541ed0f133bf62f7315db962673786842f36b
qzh78 <qzh78@uclive.ac.nz>
2022-04-17 21:23:01 +1200

    test poo

---
+++ src/Shop.java
@@ -0,0 +1,14 @@
+
+public class Shop {
+	String money;
+
+	public Shop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
